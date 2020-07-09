829c8f2365333b9badb65c1eed0eeb626a261122
Anne Maria George <67910552+annemariageorge@users.noreply.github.com>
2020-07-09 19:11:32 +0530

    Added Day1 swift code

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+// Day 1 learning
+
+var str = "Hello, playground"
+str = "End Playground"
+var money = 78
+money = 1_000_000
+
+var multiStr = """
+This goes
+multiple lines
+"""
+
+var multiStrWithoutNL = """
+This goes \
+multi line \
+without  \n
+"""
+
+var balance = 22.50
+
+var isOpen = true
+
+// String interpolation
+var score = 78
+var scoreStmt = "Your score is \(score)"
+
+let constNum = 25
+//constNum = 45 ERROR!!
+
+//Type annotation
+let album:String = "Duela Corazon"
+let year:Int = 2016
+let height:Double = 12.5
+let isAHit:Bool = true
+
